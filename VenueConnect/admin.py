c94ea8575d0072ee856ba92dc9dfe94ee46f5899
piotr067777 <01151482@pw.edu.pl>
2024-04-08 20:30:09 +0200

    register User, Venue, Review, BookingOrder to admin site

--- VenueConnect/admin.py
+++ VenueConnect/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import User, Venue, Review, BookingOrder
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Venue)
+admin.site.register(Review)
+admin.site.register(BookingOrder)
