90f18c8e33fb8ea5b52652bd4a4c52d1408fa225
piotr067777 <01151482@pw.edu.pl>
2024-04-08 21:05:47 +0200

    add email verification token generator

--- VenueConnect/utils.py
+++ VenueConnect/utils.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 class TimeRange:
@@ -11,3 +12,13 @@ class Calendar:
 
     def add_reserved_time(self, reserved_time):
         self.reserved_times.append(reserved_time)
+
+
+class TokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            str(user.pk) + str(timestamp) + str(user.email_verified)
+        )
+
+
+email_verification_token = TokenGenerator()
