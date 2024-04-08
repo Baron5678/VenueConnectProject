3fe1f569f05c95716c8d963c31638e97dd44da3f
piotr067777 <01151482@pw.edu.pl>
2024-04-08 20:30:29 +0200

    add method for creating admin users

--- VenueConnect/models.py
+++ VenueConnect/models.py
@@ -30,7 +30,14 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, email, password, **extra_fields):
-        pass
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
